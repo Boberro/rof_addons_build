6a620299a17312351b893d3ae54d8b9bc136e958
boberro <m.cyraniak@gmail.com>
2019-08-19 20:44:49 +0200

    init

---
+++ rof_addons_build/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using SwiftPbo;
+
+namespace rof_addons_build
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // ReSharper disable line StringLiteralTypo
+            const string outFolder = "@rof_addons\\addons";
+
+            foreach (var directoryPath in Directory.GetDirectories(Directory.GetCurrentDirectory()))
+            {
+                var directory = Path.GetFileName(directoryPath);
+                if (directory is null || directory.StartsWith(".") || directory.StartsWith("@")) continue;
+                Console.WriteLine(directory);
+                var outPath = Path.Combine(Directory.GetCurrentDirectory(), outFolder, directory + ".pbo");
+                PboArchive.Create(directoryPath, outPath);
+            }
+            Console.WriteLine("Done.");
+        }
+    }
+}
